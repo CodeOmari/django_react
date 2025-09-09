3961baf2174ca6e08ca59275ac04904bb50bd0aa
CodeOmari <jacksonomari2230@gmail.com>
2025-09-09 16:09:04 +0300

    update UserSerializer to allow both login and register have the same fields

--- notes_app/main_app/serializers.py
+++ notes_app/main_app/serializers.py
@@ -4,12 +4,13 @@ from .models import Note
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password1 = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+    # password1 = serializers.CharField(write_only=True)
+    # password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
         # extra_kwargs can only be used when the password field is defined in the model
         # e.g fields = ['username', 'password']
         # extra_kwargs = {
@@ -18,18 +19,18 @@ class UserSerializer(serializers.ModelSerializer):
         # }
 
 
-    def validate(self, data):
-        if data['password1'] != data['password2']:
-            raise serializers.ValidationError("Passwords do not match.")
-        return data
+    # def validate(self, data):
+    #     if data['password1'] != data['password2']:
+    #         raise serializers.ValidationError("Passwords do not match.")
+    #     return data
     
     def create(self, validated_data):
-        # user = User.objects.create_user(**validated_data) 
+        user = User.objects.create_user(**validated_data) 
         # create_user only accepts username and password
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password1']
-        )
+        # user = User.objects.create_user(
+        #     username=validated_data['username'],
+        #     password=validated_data['password1']
+        # )
         return user
     
 
