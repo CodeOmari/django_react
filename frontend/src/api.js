929b96c4b26fa42298957e0b5bd6190f896db630
CodeOmari <jacksonomari2230@gmail.com>
2026-05-05 22:15:14 +0300

    update baseURL to localhost for app testing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,12 @@ import { ACCESS_TOKEN } from "./constants";
 
 // const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL
+// });
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "http://127.0.0.1:8000",
 });
 
 api.interceptors.request.use(
