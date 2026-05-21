a74f8a5a0a7db09065aa3158c8bf18fc9cac781f
CodeOmari <jacksonomari2230@gmail.com>
2026-05-21 15:32:50 +0300

    add project functionality

---
+++ backend/user_management/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError('Passwords do not match')
+        return data
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            password = validated_data['password1']
+        )
+
+        return user
---
+++ backend/user_management/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import UserCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+urlpatterns = [
+    path('user/register/', UserCreateView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api-auth/', include('rest_framework.urls')),
+]
