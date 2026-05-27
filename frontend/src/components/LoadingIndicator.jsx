60644da39712a23bc234016c83f844d9eba59eb5
CodeOmari <jacksonomari2230@gmail.com>
2026-05-27 12:56:05 +0300

    add loader for authentication state

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,10 @@
+import "../styles/LoadingIndicator.css"
+
+
+export default function LoadingIndicator() {
+    return (
+        <div className="loading-container">
+            <div className="loader"></div>
+        </div>
+    )
+}
